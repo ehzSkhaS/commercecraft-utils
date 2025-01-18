178cd0df63aedcc352e4c7ddfa9cb7a9a53581b0
ehzSkhaS <ehzskhas@gmail.com>
2025-01-18 15:39:33 +0000

    chore: remove old translations directory
    Removed old translations directory as it has been moved to translation/
    to maintain consistent naming conventions across the project.

--- src/commercecraft_utils/translations/__init__.py
+++
@@ -1,9 +0,0 @@
-from .translation_engine import TranslationEngine
-from .translation_service import TranslationService
-from .translation_processor import TranslationProcessor
-
-__all__ = [
-    'TranslationEngine',
-    'TranslationService',
-    'TranslationProcessor',
-]
